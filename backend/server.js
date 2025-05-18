7a5da604d8e168791dc47678e5f430f3f218816b
Yohann <yohann_nicholo_matibag@dlsl.edu.ph>
2025-05-18 18:31:07 +0800

    changes by yohann

--- backend/server.js
+++ backend/server.js
@@ -5,32 +5,35 @@ const { authUser } = require("./middleware/authUser");
 
 const app = express();
 
-//Connect Database
+// Connect Database
 connectDB();
 
-//Initilize Middleware
-app.use(express.json({ extended: false }));
+// Initialize Middleware
+app.use(express.json()); // Allows Express to parse JSON request bodies
+app.use(cors()); // Enable Cross-Origin Resource Sharing
 
-app.use(cors());
-
+// Basic route for the root URL
 app.get("/", (req, res) => res.send("Server is running"));
 
-// Define routes
+// Define API routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/admin", require("./routes/api/admin"));
+app.use("/api/announcements", require("./routes/api/announcements"));
 app.use("/api/studentOrgs", require("./routes/api/studentOrgs"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/events", require("./routes/api/events"));
 
-// check if user is still authenticated
+// Check if user is still authenticated
 app.get("/authUser", authUser, (req, res) => {
-  return res.json({ msg: "Success" });
+  return res.json({ msg: "User is authenticated" });
 });
 
+// Set the server port
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, "0.0.0.0", () =>
-  console.log(`Server started on port ${PORT}`)
-);
+// Start the server
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
+});
